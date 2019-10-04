c454debcab69e6ec6f8c76188979af3d333a6118
Nigromantes <nicolasbru@hotmail.com>
2019-10-03 20:35:34 -0500

    Se dispuso del sistema para asingar color a los cuadros desde el game manager

--- Assets/Scripts/CuadroColor.cs
+++ Assets/Scripts/CuadroColor.cs
@@ -15,19 +15,20 @@ public class CuadroColor : MonoBehaviour,IPointerDownHandler {
     //    throw new NotImplementedException();
     //}
 
+    public Sprite sprite_Blanco;
     public Sprite sprite_Azul;
     public Sprite sprite_Rojo;
     public Sprite sprite_Amarillo;
 
     private SpriteRenderer spriteRender;
-    private int codigoColor;
+    private int codigoColor = 0;
     
     // Use this for initialization
 	void Awake() {
 
         spriteRender = GetComponent<SpriteRenderer>();
         //codigoColor = UnityEngine.Random.Range(1, 4);
-        codigoColor = 1; 
+        //codigoColor = 1; 
         
        AjusteColor(codigoColor);
 
@@ -39,6 +40,7 @@ public class CuadroColor : MonoBehaviour,IPointerDownHandler {
     {
         switch (codigoColor)
         {
+            case 0: spriteRender.sprite = sprite_Blanco; break;
             case 1: spriteRender.sprite = sprite_Azul; break;
             case 2: spriteRender.sprite = sprite_Rojo; break;
             case 3: spriteRender.sprite = sprite_Amarillo; break;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,149 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     // Use this for initialization
-   public CuadroColor cuadroColor;
+    //public CuadroColor cuadroColor;
+    public CuadroColor[] cuadroColor;
+    private int codigoColor;
+
+    
+    int azules; 
+    int rojos;
+    int amarillos;
+    int maximoDeCuadros;
+
+    int codigoColorAzul = 1;
+    int codigoColorRojo = 2;
+    int codigoColorAmarillo = 3;
 
 
-    void Start() {
+    void Awake()
+    {
 
-        cuadroColor.GetComponent<CuadroColor>().AjusteColor(2);
-       
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        maximoDeCuadros = cuadroColor.Length/3;
+
+         azules = maximoDeCuadros;
+         rojos = maximoDeCuadros;
+         amarillos = maximoDeCuadros;
+
+
+    }
+
+
+
+    void Start()
+    {
+
+        //cuadroColor.GetComponent<CuadroColor>().AjusteColor(2);
+
+        for (int i = 0; i < cuadroColor.Length; i++)
+        {
+
+            codigoColor = UnityEngine.Random.Range(1, 4);
+
+            switch (codigoColor)
+            {
+                case 1:
+                    if (azules > 0)
+                    {
+                       azules = AsignacionColor(i,codigoColorAzul,azules);
+                    }
+                    else if (rojos > 0)
+                    {
+                        rojos = AsignacionColor(i, codigoColorRojo, rojos);
+                    }
+                    else if (amarillos > 0)
+                    {
+
+                        amarillos = AsignacionColor(i,codigoColorAmarillo,amarillos);
+                    }
+
+                    break;
+                case 2:
+                    if (rojos > 0)
+                    {
+                        rojos = AsignacionColor(i, codigoColorRojo, rojos);
+                    }
+                    else if (amarillos > 0)
+                    {
+                        amarillos = AsignacionColor(i,codigoColorAmarillo,amarillos);
+                    }
+                    else if (azules > 0)
+                    {
+                        azules = AsignacionColor(i,codigoColorAzul,azules);
+                    }
+
+                    break;
+                case 3:
+                    if (amarillos > 0)
+                    {
+                         amarillos = AsignacionColor(i,codigoColorAmarillo,amarillos);
+                    }
+                    else if (azules > 0)
+                    {
+                        azules = AsignacionColor(i,codigoColorAzul,azules);
+                    }
+                    else if (rojos > 0)
+                    {
+                        rojos = AsignacionColor(i, codigoColorRojo, rojos);
+                    }
+
+                    break;
+            }
+
+
+        }
+    }
+
+    private int AsignacionColor(int i,  int codigoDeColor, int cantidadColores)
+    {
+        cuadroColor[i].GetComponent<CuadroColor>().AjusteColor(codigoDeColor);
+        cantidadColores--;
+        return cantidadColores;
+    }
+
+    //private void SwittchColores(int i, int codigoColor, int azules, int rojos, int amarillos)
+    //{
+    //    switch (codigoColor)
+    //    {
+    //        //case 1: cuadroColor[i].GetComponent<CuadroColor>().AjusteColor(codigoColor);
+    //        //    azules++;
+    //        //    break;
+    //        case 1:
+    //            AsignacionDecolor(i, codigoColor, azules);
+    //            break;
+    //        case 2:
+    //            AsignacionDecolor(i, codigoColor, rojos);
+    //            break;
+    //        case 3:
+    //            AsignacionDecolor(i, codigoColor, amarillos);
+    //            break;
+
+    //    }
+    //}
+
+    //void AsignacionDecolor(int i, int codigoColor, int color )
+    //{
+    //    cuadroColor[i].GetComponent<CuadroColor>().AjusteColor(codigoColor);
+    //    color++;
+
+    //}
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
