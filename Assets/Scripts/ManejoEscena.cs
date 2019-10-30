e593d3cc298a2ede68f8ea84b975dbdafdacec09
Nigromantes <nicolasbru@hotmail.com>
2019-10-30 08:04:44 -0500

    Se crearón las nuevas escenas y se crearon la base de navegación y el canvas con nuevo sistema

---
+++ Assets/Scripts/ManejoEscena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManejoEscena : MonoBehaviour {
+
+
+    public void CambioDeEscena(int escena)
+    {
+
+        SceneManager.LoadScene(escena);
+
+    }
+
+
+
+
+}
